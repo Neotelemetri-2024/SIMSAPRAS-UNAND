9febe609c14fd0ac112de768321f1522323b0e15
Habiboys <nouvalhabibie18@gmail.com>
2024-12-16 18:28:05 +0700

    Daftar Gedung di User

--- resources/views/partials/navbar-user.blade.php
+++ resources/views/partials/navbar-user.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-50 border-b border-gray-200 h-20">
    <div class="max-w-screen-xl h-full flex flex-wrap items-center justify-between mx-auto px-4">
       <!-- Logo Section remains the same -->
-      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
          <div class="bg-green-600 text-white p-2 rounded-lg">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::get('/sarana-prasarana', function () {
+    return view('sarana');
+});
 
 
 
