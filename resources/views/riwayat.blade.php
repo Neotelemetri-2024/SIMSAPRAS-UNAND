b419890b4f480db451f16e092fc6081db87f89ff
Khalied Nauly Maturino <khalidmaturino@gmail.com>
2024-12-26 18:08:42 +0700

    Pembatalan User

--- resources/views/riwayat.blade.php
+++ resources/views/riwayat.blade.php
@@ -407,7 +407,7 @@
                                 </div>
                             </div>
                         @endif
-                        @if ($pinjam->status == "diajukan" && $pinjam->alasanTolakBatal)
+                        @if ($pinjam->status == "diajukan" || $pinjam->status == "disetujui" && $pinjam->alasanTolakBatal)
                             <div class="bg-gray-50 p-4 rounded-lg border border-gray-200">
                                 <h4 class="text-lg font-semibold text-gray-900 mb-4 flex items-center">
                                     <svg class="w-5 h-5 mr-2 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none"
