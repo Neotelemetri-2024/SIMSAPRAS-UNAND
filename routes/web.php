0f7f74e510a958116f073552b5899edf3957ea13
Muhammad Nouval Habibie <nouvalhabibie18@gmail.com>
2024-12-27 00:05:05 +0700

    peminjaman oleh superadmi & pimpinan

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::middleware('auth')->group(function () {
 
 Route::group(['middleware' => ['checkRole:superadmin,pimpinan'], 'prefix' => 'admin'], function () {
     // Route yang lain...
+    Route::post('/peminjaman/store', [PeminjamanAdminController::class, 'store'])->name('peminjaman.admin.store');
     Route::put('/peminjaman/{id}/batal', [PeminjamanAdminController::class, 'batalkanPeminjaman'])->name('peminjaman.admin.batal');
 });
 // Admin Routes
